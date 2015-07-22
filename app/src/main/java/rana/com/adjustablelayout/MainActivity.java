fb2ca1985bd2ae4515ab3683a41ade1ccf1446d4
ranvijaySingh-Webonise <rana.ranvijay@weboniselab.com>
2015-07-22 15:33:42 +0530

    restructured code properly

--- app/src/main/java/rana/com/adjustablelayout/MainActivity.java
+++ app/src/main/java/rana/com/adjustablelayout/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -34,28 +35,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickAddNewView(View view){
+        /**
+         * User only one function at a time to view different demo.
+         */
+        addChipsView();
+//        addRandomView();
+//        addButtons();
+    }
+
+    /**
+     * Adding buttons
+     */
+    private void addButtons() {
+        Button button = new Button(this);
+        button.setText("Button");
+        adjustableLayout.addView(button);
+    }
+
+    /**
+     * Using view_images layout
+     */
+    private void addRandomView() {
         String name = etEnterName.getText().toString();
         if (!TextUtils.isEmpty(name)){
-
-            /**
-             * Using view_chip_text layout
-             */
-            final View newView = LayoutInflater.from(this).inflate(R.layout.view_chip_text,null);
-            TextView tvName = (TextView)newView.findViewById(R.id.tvName);
-            ImageView ivRemove = (ImageView)newView.findViewById(R.id.ivRemove);
-            ivRemove.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    adjustableLayout.removeView(newView);
-                }
-            });
-            tvName.setText(name);
-
-
-            /**
-             * Using view_images layout
-             */
-          /*
           final View newView = LayoutInflater.from(this).inflate(R.layout.view_images,null);
             TextView tvNumber = (TextView)newView.findViewById(R.id.tvNumber);
             tvNumber.setText(name);
@@ -66,8 +68,29 @@ public class MainActivity extends AppCompatActivity {
                     adjustableLayout.removeView(newView);
                 }
             });
-            */
+            tvNumber.setText(name);
+            adjustableLayout.addView(newView);
+        }else {
+            Toast.makeText(this,"Enter some text",Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    /**
+     * Using view_chip_text layout
+     */
+    private void addChipsView() {
+        String name = etEnterName.getText().toString();
+        if (!TextUtils.isEmpty(name)){
+            final View newView = LayoutInflater.from(this).inflate(R.layout.view_chip_text,null);
+            TextView tvName = (TextView)newView.findViewById(R.id.tvName);
+            ImageView ivRemove = (ImageView)newView.findViewById(R.id.ivRemove);
+            ivRemove.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    adjustableLayout.removeView(newView);
+                }
+            });
+            tvName.setText(name);
             adjustableLayout.addView(newView);
         }else {
             Toast.makeText(this,"Enter some text",Toast.LENGTH_SHORT).show();
